f5b2eb8a5d33efdea4c478e80d9a342eb1045777
wangchengnuo <1207099359@qq.com>
2021-10-12 23:32:27 +0800

    剑指 Offer Day3 字符串（简单）

---
+++ algorithm/src/String_Easy/ReplaceSpace.java
@@ -0,0 +1,15 @@
+package String_Easy;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ */
+public class ReplaceSpace {
+
+    public static String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We are happy."));
+    }
+}
---
+++ algorithm/src/String_Easy/ReverseLeftWords.java
@@ -0,0 +1,18 @@
+package String_Easy;
+
+/**
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+ * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ */
+public class ReverseLeftWords {
+
+    public static String reverseLeftWords(String s, int n) {
+        String str1 = s.substring(0, n);
+        String str2 = s.substring(n);
+        return str2+str1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseLeftWords("abcdefg", 2));
+    }
+}
