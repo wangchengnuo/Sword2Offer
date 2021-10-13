1f659ac54091d5f79c9f7c272b6fd7de1bd61a7e
wangchengnuo <1207099359@qq.com>
2021-10-13 23:56:30 +0800

    剑指 Offer Day4 查找算法（简单）

---
+++ algorithm/src/SearchAlgorithm_Easy/MissingNumber.java
@@ -0,0 +1,21 @@
+package SearchAlgorithm_Easy;
+
+/**
+ * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ */
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ algorithm/src/SearchAlgorithm_Easy/Search.java
@@ -0,0 +1,46 @@
+package SearchAlgorithm_Easy;
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ */
+public class Search {
+    public static int search(int[] nums, int target) {
+        int index = findIndex(nums, target);
+        if (index < 0 || index >= nums.length || nums[index] != target) {
+            return 0;
+        }
+        int res = 0;
+        int left = index;
+        int right = index;
+        while (left >= 0 && nums[left] == target) {
+            left--;
+            res++;
+        }
+        while (right < nums.length && nums[right] == target) {
+            right++;
+            res++;
+        }
+        return res - 1;
+    }
+
+    private static int findIndex(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{2, 2}, 3));
+        System.out.println(search(new int[]{}, 0));
+    }
+}
