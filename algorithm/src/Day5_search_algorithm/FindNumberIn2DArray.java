d292990ffecd2ea034095a5248cf3fa8d8733bac
wangchengnuo <1207099359@qq.com>
2021-10-24 23:27:10 +0800

    剑指 Offer Day5 查找算法（中等）

---
+++ algorithm/src/Day5_search_algorithm/FindNumberIn2DArray.java
@@ -0,0 +1,27 @@
+package Day5_search_algorithm;
+
+/**
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class FindNumberIn2DArray {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        // 从右上角开始 -- 关键点 下面的都比它大 左边的都比它小 可以二分
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
---
+++ algorithm/src/Day5_search_algorithm/FirstUniqChar.java
@@ -0,0 +1,30 @@
+package Day5_search_algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+ */
+public class FirstUniqChar {
+
+    public char firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            map.compute(s.charAt(i), (key, value) -> {
+                if(value == null){
+                    value = 1;
+                }else {
+                    value += 1;
+                }
+                return value;
+            });
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(map.get(s.charAt(i)) == 1){
+                return s.charAt(i);
+            }
+        }
+        return ' ';
+    }
+}
