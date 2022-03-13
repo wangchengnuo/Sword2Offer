b49141ec0408f8c83c3950580bc3293d67383bde
wangchengnuo <1207099359@qq.com>
2022-03-13 11:24:09 +0800

    Day18 搜索与回溯算法（中等）

---
+++ algorithm/src/Day18_searchBacktrace/Solution.java
@@ -0,0 +1,36 @@
+package Day18_searchBacktrace;
+
+public class Solution {
+
+    /**
+     * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+
+    /**
+     * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+     */
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    // 递归 先计算叶子节点的高度
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
