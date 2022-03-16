e4b144fb173ade31fe097ecabea560662d8c6c6f
wangchengnuo <1207099359@qq.com>
2022-03-16 23:47:22 +0800

    Day21 位运算（简单）

---
+++ algorithm/src/Day21_bitOperation/Solution.java
@@ -0,0 +1,29 @@
+package Day21_bitOperation;
+
+public class Solution {
+
+    /**
+     * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）。
+     */
+    public int hammingWeight(int n) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+     */
+    public int add(int a, int b) {
+        while (b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
